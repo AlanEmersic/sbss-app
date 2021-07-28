765592030f4505d3ab0df4261a192dab7a5a6b57
Alan <alan.emersic@gmail.com>
2021-07-28 16:08:00 +0200

    Solve week-2 part 3

--- src/main/java/com/agency04/sbss/pizza/PizzaApp.java
+++ src/main/java/com/agency04/sbss/pizza/PizzaApp.java
@@ -3,20 +3,24 @@ package com.agency04.sbss.pizza;
 import com.agency04.sbss.pizza.model.Margherita;
 import com.agency04.sbss.pizza.model.Marinara;
 import com.agency04.sbss.pizza.service.PizzaDeliveryService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@SpringBootApplication
 public class PizzaApp {
 
     public static void main(String[] args) {
         // Add some pizza code
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(PizzaConfig.class);
-
         PizzaDeliveryService fianonaPizzaDeliveryService = context.getBean("fianona", PizzaDeliveryService.class);
         System.out.println(fianonaPizzaDeliveryService.orderPizza(new Margherita()));
         PizzaDeliveryService capucinerPizzaDeliveryService = context.getBean("capuciner", PizzaDeliveryService.class);
         System.out.println(capucinerPizzaDeliveryService.orderPizza(new Marinara()));
 
         context.close();
+
+        SpringApplication.run(PizzaApp.class, args);
     }
 
 }
